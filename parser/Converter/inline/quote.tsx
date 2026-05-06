5fd66b1542923118b9ffb89d965a47a6f8743854
Simon Mika <simon@mika.se>
2026-05-06 19:01:28 +0200

    Added support for frame, link flags and block and inline quotes.

--- parser/Converter/inline/quote.tsx
+++ parser/Converter/inline/quote.tsx
@@ -2,6 +2,6 @@ import { h } from "@stencil/core"
 import { dom } from "@typeup/dom"
 import { convert, register } from "../convert"
 
-register("inline.emphasize", async (node: dom.Inline.Emphasize) => {
-	return <em>{await convert(node.content)}</em>
+register("inline.quote", async (node: dom.Inline.Quote) => {
+	return <q>{await convert(node.content)}</q>
 })
